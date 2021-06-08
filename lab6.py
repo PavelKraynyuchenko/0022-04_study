79d8936fa7625cd7903deb1ecb78525b5c720412
PavelKraynyuchenko <78962028+PavelKraynyuchenko@users.noreply.github.com>
2021-06-08 19:15:17 +0300

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -1,8 +1,7 @@
 list_1 = []
 list_3 = []
 
-
-def completion_mass_key():
+def mass_key():
     str = []
     while (True):
         c = input()
@@ -12,22 +11,20 @@ def completion_mass_key():
             str.append(c)
     return str
 
-
-def completion_mass_values(str):
+def mass_values(str):
     str_3 = []
     for i in range(len(str)):
         c = input()
         str_3.append(c)
     return str_3
 
-
+#completion-завершение
 def completion_list(str_3, str):
     s = {str[i]: str_3[i] for i in range(len(str))}
     return s
 
-
 print("quit -выход из цикла")
-list_1 = completion_mass_key()
+list_1 = mass_key()
 
 list_2 = set(list_1)
 print("список -", list_1)
@@ -35,7 +32,7 @@ print("множество - ", list_2)
 print("количество символов в списке =", len(list_1))
 print("заполните следующий список из", len(list_1), "символов")
 
-list_3 = completion_mass_values(list_1)
+list_3 = mass_values(list_1)
 
 print(list_3)
 list = completion_list(list_3, list_1)
