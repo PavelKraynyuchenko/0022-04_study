31fb2b56376bfb54aaf33b56d3ca9718cb0cb83e
PavelKraynyuchenko <78962028+PavelKraynyuchenko@users.noreply.github.com>
2021-06-08 19:10:10 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,13 +1,13 @@
 def main():
- x=int(input('введите произвольное число'))
- y=int(input('введите пограничное число'))
- if x>y:
-     if x>y*3:
+ number1=int(input('введите произвольное число'))
+ number2=int(input('введите пограничное число'))
+ if number1>number2:
+     if number1>number2*3:
          print("произвольное число больше пограничного в 3 раза")
      else:
          print("произвольное число больше пограничного")
- if x<y:
+ if number1<number2:
      print("произвольное число меньше пограничного")
- if x==y:
+ if number1==number2:
      print("числа равны")
 main()
