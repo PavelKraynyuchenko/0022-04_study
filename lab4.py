2199baf239453f5199c4aec42ee52f0beb2d1730
PavelKraynyuchenko <78962028+PavelKraynyuchenko@users.noreply.github.com>
2021-06-08 19:13:45 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,29 +1,40 @@
 print("введите 3 названия овоща")
 print("первый овощ")
-z = input()
+ovosch1 = input()
 print("вторый овощ")
-x = input()
+ovosch2 = input()
 print("третий овощ")
-c = input()
-z1 = z.lower()
-x1 = x.lower()
-c1 = c.lower()
-z2 = z.upper()
-x2 = x.upper()
-c2 = c.upper()
-z3 = z.capitalize()
-x3 = x.capitalize()
-c3 = c.capitalize()
+ovosch3 = input()
+
+z1 = ovosch1.lower()
+x1 = ovosch2.lower()
+c1 = ovosch3.lower()
+
+z2 = ovosch1.upper()
+x2 = ovosch2.upper()
+c2 = ovosch3.upper()
+
+z3 = ovosch1.capitalize()
+x3 = ovosch2.capitalize()
+c3 = ovosch3.capitalize()
+
 print(z1,x1,c1)
 print(z2,x2,c2)
 print(z3,x3,c3)
+
 print("введите количество первого овоща=")
-z4 =int(input())
+count1 =int(input())
 print("введите количество второго овоща=")
-x4 =int(input())
+count2 =int(input())
 print("введите количество третьего овоща=")
-c4 =int(input())
-print("количество овощей: {0}-{1},{2}-{3},{4}-{5}".format(z,z4,x,x4,c,c4))
+count3 =int(input())
+
+print("количество овощей: {0}-{1},{2}-{3},{4}-{5}".format(ovosch1,count1,ovosch2,count2,ovosch3,count3))
+
+
+
+
+
 
 
 
